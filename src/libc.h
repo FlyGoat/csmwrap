abfaa3d3dc6497a2f620ca044b34165874008ef9
Jiaxun Yang <jiaxun.yang@flygoat.com>
2025-05-19 14:57:33 +0100

    Use builtin memcpy functions
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>

--- src/libc.h
+++ src/libc.h
@@ -8,4 +8,9 @@ void *memset(void *s, int c, size_t n);
 void *memmove(void *dest, const void *src, size_t n);
 int memcmp(const void *s1, const void *s2, size_t n);
 
+/* Access builtin version by default. */
+#define memcpy(d,s,l) __builtin_memcpy(d,s,l)
+#define memset(d,c,l) __builtin_memset(d,c,l)
+#define memcmp	__builtin_memcmp
+
 #endif
