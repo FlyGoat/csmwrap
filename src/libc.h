a25d1ee9736230d2d13ca080161a9b89b8e90991
Mintsuki <mintsuki@protonmail.com>
2025-05-19 20:02:07 +0200

    Ensure that mem*() family function implementations are not shadowed by macros
    This also unbreaks the Clang build.

--- src/libc.h
+++ src/libc.h
@@ -9,8 +9,9 @@ void *memmove(void *dest, const void *src, size_t n);
 int memcmp(const void *s1, const void *s2, size_t n);
 
 /* Access builtin version by default. */
-#define memcpy(d,s,l) __builtin_memcpy(d,s,l)
-#define memset(d,c,l) __builtin_memset(d,c,l)
-#define memcmp	__builtin_memcmp
+#define memcpy __builtin_memcpy
+#define memset __builtin_memset
+#define memmove __builtin_memmove
+#define memcmp __builtin_memcmp
 
 #endif
